98b51317b011fe24f4b58cc444c9efd0b24d2b6b
Kyle Mulka <repalviglator@yahoo.com>
2014-03-14 16:27:34 -0400

    added random 8-ball responses to SMS

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask
 import twilio.twiml
 
@@ -9,8 +11,32 @@ def hello():
 
 @app.route("/sms", methods=["GET", "POST"])
 def sms():
+    messages = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful",
+    ]
+    message = random.choice(messages)
+
     resp = twilio.twiml.Response()
-    resp.message("Hello!")
+    resp.message(message)
     return str(resp)
 
 if __name__ == "__main__":
