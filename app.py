d81161d98181a1dd31b16fbfc021d68fecbb3ebd
Kyle Mulka <repalviglator@yahoo.com>
2014-03-14 16:25:10 -0400

    added TwiML response to /sms

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import twilio.twiml
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/sms", methods=["GET", "POST"])
+def sms():
+    resp = twilio.twiml.Response()
+    resp.message("Hello!")
+    return str(resp)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
